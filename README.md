2c5a2e9edf62dd24c187ab376d7a4a80bbddddd1
sfletc <steveofbrisbane@gmail.com>
2023-06-01 10:50:05 +1000

    removed adaptTrim and simplified CLI

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
 ## The scramAligner pipeline
 
-The scramAligner pipeline is designer to align small RNA reads to references sequences with no mis-matches.  There is no requirement for reference index building.  The pipeline is ideally suited to analysis of siRNAs generated from transgenes or viruses.  Alignment files are generated for discrete read lengths (e.g. 21, 22 and 24nt).  Along with the core aligner (scram2), command line scripts for adapter trimming (adaptTrim) and alignment plotting are provided.  
+The scramAligner pipeline is designer to align small RNA reads to references sequences with no mis-matches.  There is no requirement for reference index building.  The pipeline is ideally suited to analysis of siRNAs generated from transgenes or viruses.  Alignment files are generated for discrete read lengths (e.g. 21, 22 and 24nt).  Along with the core aligner (scram2), a command line script for alignment plotting is provided.  
 
 ----
 
 ### Installation
 
-- TODO
+
+
+
 
 ### Usage
 
 NAME
-scram2- a command line Golang-based small RNA exact matching aligner
+scramAligner- a command line Golang-based small RNA exact matching aligner
 
 SYNOPSIS
-scram2 align [OPTIONS]
+scramAligner -r <reference> -f <fileSet> -l <length> -o <outFilePrefix> [flags]
+
 
 DESCRIPTION
 The SCRAM Aligner is a Golang-based command line application that performs exact matching alignment of small RNA sequences against a reference FASTA file. It is a core component of the SCRAM pipeline for small RNA exact matching alignment.
@@ -23,11 +26,12 @@ OPTIONS
 -h, --help
 Display the help message and exit.
 
+FLAGS
 ```-r, --alignTo <path>
     Path to the FASTA reference file.
 
--1, --fastxSet1 <path>
-    Comma-separated path to the read file set 1 (collapsed FASTA, FASTA, or FASTQ). GZIPped files must have .gz file extension.
+-f, --fastxSet <path>
+    Comma-separated path to the read file or set of read file replicates (collapsed FASTA, FASTA, or FASTQ). GZIPped files must have .gz file extension.
 
 -t, --readFileType <type>
     Read file type: cfa (collapsed FASTA), fa (FASTA), fq (FASTQ) (default: fq).
@@ -58,5 +62,7 @@ Display the help message and exit.
 ```
 
 EXAMPLES
-scram2 align -r reference.fa -1 reads.fq -l 21,22,24 -o out
+```
+scramAligner -r reference.fa -f reads.fq.gz -l 21,22,24 -o out
+```
 
--- main.go
+++ main.go
@@ -30,8 +30,92 @@
 
 package main
 
-import "scramAligner/cmd"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	scramPkg "scramAligner/scramPkg"
+
+	"github.com/spf13/cobra"
+)
+
+const version = "2.1.0"
+
+var cfgFile string
+var fastaSet1 string
+var readFileType string
+var length string
+var alignTo string
+var outFilePrefix string
+var noSplit bool
+var minLen int
+var maxLen int
+var minCount float64
+var adapter string
+var noNorm bool
+
+var rootCmd = &cobra.Command{
+	Use:   "scramAligner -r <reference> -f <fileSet> -l <length> -o <outFilePrefix>",
+	Short: "Exact match small RNA read alignment v" + version,
+	Long:  `Exact match small RNA read alignment v` + version,
+	Run:   align,
+}
 
 func main() {
-	cmd.Execute()
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+
+	rootCmd.PersistentFlags().StringVarP(&alignTo, "alignTo", "r", "", "path/to/FASTA reference file - REQUIRED")
+	rootCmd.MarkFlagRequired("alignTo")
+	rootCmd.PersistentFlags().StringVarP(&fastaSet1, "fastxSet", "f", "", "comma-separated path/to/read file set 1. GZIPped files must have .gz file extension - REQUIRED")
+	rootCmd.MarkFlagRequired("fastxSet")
+	rootCmd.PersistentFlags().StringVarP(&readFileType, "readFileType", "t", "fq", "Read file type: cfa (collapsed FASTA), fa (FASTA), fq (FASTQ), clean (BGI clean.fa).")
+	rootCmd.PersistentFlags().StringVarP(&length, "length", "l", "", "comma-separated read (sRNA) lengths to align - REQUIRED")
+	rootCmd.MarkFlagRequired("length")
+	rootCmd.PersistentFlags().StringVarP(&outFilePrefix, "outFilePrefix", "o", "", "path/to/outfile prefix (len.csv will be appended) - REQUIRED")
+	rootCmd.MarkFlagRequired("outFilePrefix")
+	rootCmd.PersistentFlags().BoolVar(&noSplit, "noSplit", false, "Do not split alignment count for each read by the number of times it aligns")
+	rootCmd.PersistentFlags().BoolVar(&noNorm, "noNorm", false, "Do not normalize read counts by library size (i.e. reads per million reads)")
+	rootCmd.PersistentFlags().IntVar(&minLen, "minLen", 18, "Minimum read length to include for RPMR normalization")
+	rootCmd.PersistentFlags().IntVar(&maxLen, "maxLen", 32, "Maximum read length to include for RPMR normalization")
+	rootCmd.PersistentFlags().Float64Var(&minCount, "minCount", 1.0, "Minimum read count for alignment and to include for RPMR normalization")
+	rootCmd.PersistentFlags().StringVar(&adapter, "adapter", "nil", "3' adapter sequence to trim - FASTA & FASTQ only")
+
+}
+
+func align(cmd *cobra.Command, args []string) {
+	if readFileType != "cfa" && readFileType != "fa" && readFileType != "fq" && readFileType != "clean" {
+		fmt.Println("\nCan't parse read file type " + readFileType)
+		os.Exit(1)
+	}
+	t0 := time.Now()
+	var a map[string]interface{}
+	var fileOrder []string
+
+	fmt.Println("\nLoading individual read counts")
+	a, fileOrder = scramPkg.IndvSeqLoad(strings.Split(fastaSet1, ","), readFileType, adapter, minLen, maxLen, minCount, noNorm)
+
+	fmt.Println("\nLoading reference")
+	c := scramPkg.RefLoad(alignTo)
+	for _, nt := range strings.Split(length, ",") {
+		nt, _ := strconv.Atoi(nt)
+		fmt.Printf("\nAligning %v nt reads\n", nt)
+		d := scramPkg.AlignReads(a, c, nt)
+		switch {
+		case !noSplit:
+			e := scramPkg.ProfileSplit(d, a)
+			scramPkg.ProfileToCsv(e, c, nt, outFilePrefix, fileOrder)
+		default:
+			e := scramPkg.ProfileNoSplit(d, a)
+			scramPkg.ProfileToCsv(e, c, nt, outFilePrefix, fileOrder)
+
+		}
+	}
+	t1 := time.Now()
+	fmt.Printf("\nAlignment complete.  Total time taken = %s\n", t1.Sub(t0))
 }
